62bd944abed050cbbb6255b15cf15a54cc0d83e6
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-07 11:25:53 +0500

    initialized listOfTasks with array of objects and import the class from modules and call the function to show the list of tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import _ from 'lodash';
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
+import ToDoTasks from '../modules/toDoTasks';
+
+const listOftasks = [
+  {
+    description: "Double-Tap to edit.",
+    completed: false,
+    index: 1
+  },
+  {
+    description: "Drag in drop to reorder your list.",
+    completed: false,
+    index: 2
+  },
+  {
+    description: "Resync to clear out the old.",
+    completed: false,
+    index: 4
+  },
+  {
+    description: "Manage All your list in one place.",
+    completed: false,
+    index: 3
+  }
+];
+listOftasks.sort((task1, task2) => task1.index - task2.index);
+const toDoTasks = new ToDoTasks(listOftasks);
+toDoTasks.displayLists();
+const checkboxes = document.querySelectorAll('.checkbox');
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', toDoTasks.checkBoxClick);
+});
