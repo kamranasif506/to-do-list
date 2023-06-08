231de84015cca04273d122881b167e8da82594d4
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-08 09:39:43 +0500

    Change the to-do-list dynamically fetched from localStorage and use addToList function to add new task to the list and clearList function to remove all the tasks

--- modules/toDoTasks.js
+++ modules/toDoTasks.js
@@ -1,17 +1,21 @@
 class ToDoTasks {
-  constructor(toDoList) {
+  constructor(taskList) {
     this.toDoListsDiv = document.getElementById('to-do-lists');
-    this.toDoList = toDoList;
-    this.list = '';
+    this.toDoList = taskList;
+    this.addListField = document.getElementById('addList');
+    this.inputListitem = document.getElementById('addList');
+    
   }
 
-  displayLists() {
-    this.toDoList.forEach((task) => {
+  displayLists(taskList) {
+    this.list = '';
+    Object.values(taskList).forEach((task) => {
+       
       if (task.completed) {
         this.list += ` <li data-index="${task.index}">
                                 <div class="list-item">
                                     <i class="fas fa-check checked" id="checkbox${task.index}" ></i> 
-                                    <input  value="${task.description}" class="list-text underline">
+                                    <input  value="${task.value}" class="list-text underline">
                                     <i class="fas fa-trash list-icon fa-ellipsis-vertical"></i>
                                 </div>
                             </li>`;
@@ -20,22 +24,80 @@ class ToDoTasks {
                                 <div class="list-item">
                                     <input type="checkbox" id="checkbox${task.index}" class="checkbox"> 
                                     <label for="checkbox${task.index}"></label>
-                                    <input  value="${task.description}" class="list-text">
+                                    <input  value="${task.value}" class="list-text">
                                     <i class="fas fa-sharp list-icon fa-ellipsis-vertical"></i>
                                 </div>
                             </li>`;
       }
     });
+    this.toDoListsDiv.innerHTML = '';
     this.toDoListsDiv.innerHTML = this.list;
-  }
+        
+    
 
+  }
+  updateBookData(collectionData) {
+    localStorage.setItem('tasksList', JSON.stringify(collectionData));
+    this.addListField.value = '';
+    
+  }
+  addToList(value,completed,index) {
+    this.toDoList.push({value,completed,index});
+    this.updateBookData(this.toDoList); 
+    this.displayLists(this.toDoList);               
+  }
   checkBoxClick(event) {
     this.checkbox = event.target;
     this.checkboxId = this.checkbox.getAttribute('id');
-    this.checkboxIcon = `<i class="fas fa-check checked" id="${this.checkboxId}"></i>`;
-
+    this.checkboxIcon = `<i class="fas fa-check checked" data-cbecked="true" id="${this.checkboxId}"></i>`;
     this.checkbox.style.display = 'none';
     this.checkbox.nextElementSibling.outerHTML = this.checkboxIcon;
+    this.parent = this.checkbox.parentElement;
+    this.parent.querySelector('.list-text').classList.add('underline');
+    const index  = this.parent.parentElement.getAttribute('data-index');
+
+    const checkedIcon = this.parent.querySelector('.checked');
+    checkedIcon.addEventListener('click', (event) => this.checkedIconClick(event));
+  
+  }
+  checkedIconClick(event) {
+    const checkedIcon = event.target;
+    const checkboxId = checkedIcon.getAttribute('id');
+    const checkbox = document.getElementById(checkboxId);
+    checkbox.style.display = 'block';
+    checkedIcon.remove();
+  }
+  editList(index,value){
+    const updatedTasks = this.toDoList.filter(tasks => {
+        if (tasks.index === parseInt(index)) {
+          tasks.value = value;
+          return true;
+        }
+        return true;
+      });
+    this.toDoList = updatedTasks;  
+    this.updateBookData(this.toDoList);
+  }
+  removeListitem(index){
+        
+    this.toDoList = this.toDoList.filter(task => {
+      return task.index !== parseInt(index);
+    });
+
+    this.toDoList = this.toDoList.map((task, index) => {
+      task.index = index + 1;
+      return task;
+    });
+    this.updateBookData(this.toDoList);
+  }
+  updateLists() {
+    
+    this.displayLists(this.toDoList);
+  }
+  clearList(){
+    localStorage.removeItem('tasksList');
+    this.toDoList = [];
+    this.displayLists(this.toDoList);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,32 +2,34 @@ import './style.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import ToDoTasks from '../modules/toDoTasks.js';
 
-const listOftasks = [
-  {
-    description: 'Double-Tap to edit.',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Drag in drop to reorder your list.',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Resync to clear out the old.',
-    completed: false,
-    index: 4,
-  },
-  {
-    description: 'Manage All your list in one place.',
-    completed: false,
-    index: 3,
-  },
-];
-listOftasks.sort((task1, task2) => task1.index - task2.index);
-const toDoTasks = new ToDoTasks(listOftasks);
-toDoTasks.displayLists();
+const taskList = JSON.parse(localStorage.getItem('tasksList')) || [];
+const toDoTasks = new ToDoTasks(taskList);
+toDoTasks.displayLists(taskList);
 const checkboxes = document.querySelectorAll('.checkbox');
 checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('click', (event) => toDoTasks.checkBoxClick(event));
+  // checkbox.addEventListener('click', (event) => toDoTasks.checkBoxClick(event));
 });
+
+
+const addListField = document.getElementById('addList');
+const addListBtn = document.getElementById('addListBtn');
+const toDoListsDiv = document.getElementById('to-do-lists');
+const clearlist = document.getElementById('clear-list');
+addListBtn.addEventListener('click', () => {
+  let childCount = toDoListsDiv.childElementCount; 
+  const value = addListField.value;
+  childCount = childCount + 1;
+  toDoTasks.addToList(value,false,childCount);
+})
+addListField.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    let childCount = toDoListsDiv.childElementCount; 
+    const value = addListField.value;
+    childCount = childCount + 1;
+    console.log(childCount);
+    toDoTasks.addToList(value,false,childCount);
+  }
+});
+clearlist.addEventListener('click', () => {
+  toDoTasks.clearList();
+})
