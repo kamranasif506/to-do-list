04bd2f828d5edfbcb9c7e4679872587e8e7627b1
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-14 21:59:02 +0500

    resolved linter issues and removed commented lines

--- modules/updateBookData.js
+++ modules/updateBookData.js
@@ -2,7 +2,6 @@ export default function updateBookData(collectionData) {
   localStorage.setItem('tasksList', JSON.stringify(collectionData));
   const tasksList = JSON.parse(localStorage.getItem('tasksList'));
 
-  // Clear the value of the input field with id 'addList'
   const addListField = document.getElementById('addList');
   if (addListField) {
     addListField.value = '';
--- toDoList.test.js
+++ toDoList.test.js
@@ -8,7 +8,7 @@ jest.mock('./modules/updateBookData.js');
 jest.mock('./modules/displayLists.js');
 
 describe('addToList', () => {
-    let toDoList;
+  let toDoList;
 
   beforeEach(() => {
     // Set up the initial to-do list
@@ -17,7 +17,7 @@ describe('addToList', () => {
       { value: 'Task 2', completed: false, index: 2 },
     ];
   });
-  
+
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -40,49 +40,49 @@ describe('addToList', () => {
 });
 
 describe('removeListitem', () => {
-    let toDoList;
-  
-    beforeEach(() => {
-      // Set up the initial to-do list
-      toDoList = [
-        { index: 1, task: 'Task 1' },
-        { index: 2, task: 'Task 2' },
-        { index: 3, task: 'Task 3' },
-      ];
-    });
-  
-    afterEach(() => {
-      // Clear the mock calls after each test
-      jest.clearAllMocks();
-    });
-  
-    it('removes the specified item from the to-do list', () => {
-      const indexToRemove = 2;
-  
-      const updatedList = removeListitem(indexToRemove, toDoList);
-  
-      // Verify that the item is removed from the list
-      expect(updatedList).toEqual([
-        { index: 1, task: 'Task 1' },
-        { index: 2, task: 'Task 3' },
-      ]);
-  
-      // Verify that the updateBookData function is called with the updated list
-      expect(updateBookData).toHaveBeenCalledWith(updatedList);
-    });
-  
-    it('reindexes the tasks after removing an item', () => {
-      const indexToRemove = 1;
-  
-      const updatedList = removeListitem(indexToRemove, toDoList);
-  
-      // Verify that the tasks are reindexed correctly
-      expect(updatedList).toEqual([
-        { index: 1, task: 'Task 2' },
-        { index: 2, task: 'Task 3' },
-      ]);
-  
-      // Verify that the updateBookData function is called with the updated list
-      expect(updateBookData).toHaveBeenCalledWith(updatedList);
-    });
+  let toDoList;
+
+  beforeEach(() => {
+    // Set up the initial to-do list
+    toDoList = [
+      { index: 1, task: 'Task 1' },
+      { index: 2, task: 'Task 2' },
+      { index: 3, task: 'Task 3' },
+    ];
   });
+
+  afterEach(() => {
+    // Clear the mock calls after each test
+    jest.clearAllMocks();
+  });
+
+  it('removes the specified item from the to-do list', () => {
+    const indexToRemove = 2;
+
+    const updatedList = removeListitem(indexToRemove, toDoList);
+
+    // Verify that the item is removed from the list
+    expect(updatedList).toEqual([
+      { index: 1, task: 'Task 1' },
+      { index: 2, task: 'Task 3' },
+    ]);
+
+    // Verify that the updateBookData function is called with the updated list
+    expect(updateBookData).toHaveBeenCalledWith(updatedList);
+  });
+
+  it('reindexes the tasks after removing an item', () => {
+    const indexToRemove = 1;
+
+    const updatedList = removeListitem(indexToRemove, toDoList);
+
+    // Verify that the tasks are reindexed correctly
+    expect(updatedList).toEqual([
+      { index: 1, task: 'Task 2' },
+      { index: 2, task: 'Task 3' },
+    ]);
+
+    // Verify that the updateBookData function is called with the updated list
+    expect(updateBookData).toHaveBeenCalledWith(updatedList);
+  });
+});
