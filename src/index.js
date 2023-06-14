9cce39fb5ab071b5a211c673a040cc976003ea94
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-06-14 14:53:31 +0500

    Resolved issues suggested by other CRs

--- src/index.js
+++ src/index.js
@@ -18,7 +18,10 @@ addListBtn.addEventListener('click', () => {
   const { value } = addListField;
   childCount += 1;
   const refreshTaskList = JSON.parse(localStorage.getItem('tasksList')) || [];
-  addToList(value, false, childCount, refreshTaskList);
+  if(value !== ''){
+    addToList(value, false, childCount, refreshTaskList);
+  }
+  
 });
 addListField.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
@@ -26,7 +29,11 @@ addListField.addEventListener('keydown', (event) => {
     const { value } = addListField;
     childCount += 1;
     const refreshTaskList = JSON.parse(localStorage.getItem('tasksList')) || [];
-    addToList(value, false, childCount, refreshTaskList);
+
+    if(value !== ''){
+      addToList(value, false, childCount, refreshTaskList);
+    }
+    
   }
 });
 clearlist.addEventListener('click', () => {
